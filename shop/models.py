40ff7a2266b44a5bfd399122c6e9332b803512ec
ranachkov <ranachkov@gmail.com>
2019-05-15 13:00:36 +0300

    First commit

---
+++ shop/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Question(models.Model):
+    author = models.CharField(max_length=200, default='Anonymous')
+    question = models.TextField()
+
+
+    def __str__(self):
+        return f"{self.author} {self.question}"
+
+
+class Answer(models.Model):
+    author = models.CharField(max_length=200, default='Anonymous')
+    contend = models.TextField()
+    likes = models.PositiveIntegerField()
+    dislikes = models.PositiveIntegerField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.author} {self.contend[:10]}..."
+
---
+++ shop/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Question
+from .serialisers import QuestionSerializer
+
+
+class QustionsList(APIView):
+
+    def get(self, request):
+        questions = Question.objects.all()
+        serializer = QuestionSerializer(questions, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = QuestionSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
